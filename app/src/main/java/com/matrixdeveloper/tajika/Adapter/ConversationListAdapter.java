e8a2f6e0d4d8b77ba5817657a3dc9a01c43bdfdd
Shiv Kumar <shiukumar05@gmail.com>
2021-02-09 21:15:22 +0530

    Book Service,ConversationList, AboutUs, Privacy Policy Screens Added

---
+++ app/src/main/java/com/matrixdeveloper/tajika/Adapter/ConversationListAdapter.java
@@ -0,0 +1,56 @@
+package com.matrixdeveloper.tajika.Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.matrixdeveloper.tajika.Model.ConversationListModel;
+import com.matrixdeveloper.tajika.R;
+
+public class ConversationListAdapter extends RecyclerView.Adapter<ConversationListAdapter.viewModel> {
+
+    private Context ctx;
+    private ConversationListModel[] conversationListModels;
+
+    public ConversationListAdapter(Context ctx, ConversationListModel[] conversationListModels) {
+        this.ctx = ctx;
+        this.conversationListModels = conversationListModels;
+    }
+
+    @NonNull
+    @Override
+    public viewModel onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_conversation_list, parent, false);
+        return new ConversationListAdapter.viewModel(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewModel holder, int position) {
+        final ConversationListModel listModel = conversationListModels[position];
+        holder.serviceText.setText(listModel.getHeader());
+        holder.serviceDate.setText(listModel.getTime());
+    }
+
+    @Override
+    public int getItemCount() {
+        return conversationListModels.length;
+    }
+
+    public static class viewModel extends RecyclerView.ViewHolder {
+        private ImageView serviceImage;
+        private TextView serviceText, serviceDate;
+
+        public viewModel(@NonNull View itemView) {
+            super(itemView);
+            serviceImage = itemView.findViewById(R.id.iv_serviceImage);
+            serviceText = itemView.findViewById(R.id.txt_serviceType);
+            serviceDate = itemView.findViewById(R.id.txt_serviceDate);
+        }
+    }
+}
---
+++ app/src/main/java/com/matrixdeveloper/tajika/ConversationListActivity.java
@@ -0,0 +1,38 @@
+package com.matrixdeveloper.tajika;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.matrixdeveloper.tajika.Adapter.ConversationListAdapter;
+import com.matrixdeveloper.tajika.Adapter.VoucherAdapter;
+import com.matrixdeveloper.tajika.Model.ConversationListModel;
+import com.matrixdeveloper.tajika.Model.VoucherModel;
+
+public class ConversationListActivity extends AppCompatActivity {
+
+    private RecyclerView conversationListRec;
+    private ConversationListAdapter myAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conversation_list);
+
+
+        ConversationListModel[] convModel = new ConversationListModel[]{
+                new ConversationListModel(1,R.drawable.app_logo, "Super Catering", "Yesterday"),
+                new ConversationListModel(2,R.drawable.app_logo, "E sewa", "Today"),
+                new ConversationListModel(3,R.drawable.app_logo, "Plumbing", "Tomorrow"),
+
+        };
+
+        conversationListRec = findViewById(R.id.revView_conversationList);
+        myAdapter = new ConversationListAdapter(this, convModel);
+        conversationListRec.setHasFixedSize(true);
+        conversationListRec.setLayoutManager(new LinearLayoutManager(this));
+        conversationListRec.setAdapter(myAdapter);
+    }
+}
