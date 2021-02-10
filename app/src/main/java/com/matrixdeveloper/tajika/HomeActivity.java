533b7e9041e486fd8a101100ff005d041520e643
Shiv Kumar <shiukumar05@gmail.com>
2021-02-10 22:40:22 +0530

    navigation drawer , home screen half added

--- app/src/main/java/com/matrixdeveloper/tajika/HomeActivity.java
+++ app/src/main/java/com/matrixdeveloper/tajika/HomeActivity.java
@@ -1,20 +1,26 @@
 package com.matrixdeveloper.tajika;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 public class HomeActivity extends AppCompatActivity {
+    DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        drawer = findViewById(R.id.drawer_layout);
+
     }
 
-    public void onMyPRofileClick(View view) {
-        startActivity(new Intent(this, MyProfileActivity.class));
+    public void onNavDrawerClick(View view) {
+        if (!drawer.isDrawerOpen(Gravity.START)) drawer.openDrawer(Gravity.START);
+        else drawer.closeDrawer(Gravity.END);
     }
 }
