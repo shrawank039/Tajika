96c14883a4e7ab8db05f490951f2467c7628925d
shrawan <shrawank039@gmail.com>
2021-03-18 21:47:53 +0530

    FCM Added

---
+++ app/src/main/java/com/matrixdeveloper/tajika/model/BasicBean.kt
@@ -0,0 +1,62 @@
+package com.matrixdeveloper.tajika.model
+
+/**
+ * Created by AkshCabs.com on 03 December, 2016.
+ * Package com.matrixdeveloper.akshdriver.model
+ * Project LaTaxi
+ */
+
+open class BasicBean : BaseBean() {
+
+
+    /* private ArrayList<CountryBean> countries;
+    private ArrayList<StateBean> states;
+    private ArrayList<DistrictBean> cities;
+    private ArrayList<CourtBean> courts;*/
+
+    var requestID: String= ""
+    var tripID: String= ""
+    var tripStatus: Int = 0
+    var otpCode: String= ""
+    var phone: String= ""
+
+    var countryID: Int = 0
+    var stateID: Int = 0
+    var districtID: Int = 0
+
+    var isDriverOnline: Boolean = false
+    var isPhoneAvailable: Boolean = false
+    var statusCode: String =""
+    /*
+    public ArrayList<CourtBean> getCourts() {
+        return courts;
+    }
+
+    public void setCourts(List<CourtBean> courts) {
+        this.courts = courts;
+    }
+
+    public ArrayList<CountryBean> getCountries() {
+        return countries;
+    }
+
+    public void setCountries(List<CountryBean> countries) {
+        this.countries = countries;
+    }
+
+    public ArrayList<StateBean> getStates() {
+        return states;
+    }
+
+    public void setStates(List<StateBean> states) {
+        this.states = states;
+    }
+
+    public ArrayList<DistrictBean> getDistricts() {
+        return cities;
+    }
+
+    public void setDistricts(List<DistrictBean> cities) {
+        this.cities = cities;
+    }*/
+}
